d893dff2ade12e7cfc20cd5bea9d9b10dc1788cc
Varun Sharma <varun@sharmalabs.org>
2015-07-03 15:41:12 +0530

    Update data api for NewTryClient by adding new method get to fetch data from db

--- master/buildbot/data/tryclients.py
+++ master/buildbot/data/tryclients.py
@@ -1,5 +1,26 @@
+# This file is part of Buildbot.  Buildbot is free software: you can
+# redistribute it and/or modify it under the terms of the GNU General Public
+# License as published by the Free Software Foundation, version 2.
+#
+# This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+# FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+# details.
+#
+# You should have received a copy of the GNU General Public License along with
+# this program; if not, write to the Free Software Foundation, Inc., 51
+# Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+#
+# Copyright Buildbot Team Members
+
 from buildbot.data import base
 from buildbot.data import types
+from buildbot.clients import NewTryClient
+from twisted.internet import defer
+
+
+class tryClient2Data(tc):
+    pass
 
 
 class TryClientEndpoint(base.Endpoint):
@@ -9,9 +30,16 @@ class TryClientEndpoint(base.Endpoint):
         /tryclient/i:tryclientid
     """
 
+    @defer.inlineCallbacks
     def get(self, resultSpec, kwargs):
-        return 'hi'
-#        return self.master.db.tryclient.getTryClient(kwargs['tryclientid'])
+        bdicts = yield self.master.db.tryclients.getTryClient(
+            tryclientid=kwargs.get('tryclientid', None))
+        defer.returnValue([
+            dict(tryclientid=bd['tryclientid'],
+                 name=bd['name'],
+                 repo=bd['repo'],
+                 diff=bd['diff'])
+            for bd in bdicts])
 
 
 class TryClient(base.ResourceType):
@@ -20,9 +48,7 @@ class TryClient(base.ResourceType):
     keyFields = ['tryclientid']
 
     class EntityType(types.Entity):
-        tryclientid = types.Integer()
-        name = types.String()
-        label = types.String()
+        tryclientid = types.Identifier()
         repo = types.String()
-        patch = types.String()
+        diff = types.String()
     entityType = EntityType(name)
